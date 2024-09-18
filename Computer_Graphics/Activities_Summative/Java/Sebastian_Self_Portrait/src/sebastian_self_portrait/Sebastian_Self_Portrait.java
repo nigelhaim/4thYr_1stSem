a39734d878246ac75bb5234bcb9f314b3e4888d5
Nigel Sebastian <nigelsebastian21@gmail.com>
2024-09-18 17:24:27 +0800

    Missing head

--- Computer_Graphics/Activities_Summative/Java/Sebastian_Self_Portrait/src/sebastian_self_portrait/Sebastian_Self_Portrait.java
+++ Computer_Graphics/Activities_Summative/Java/Sebastian_Self_Portrait/src/sebastian_self_portrait/Sebastian_Self_Portrait.java
@@ -17,9 +17,26 @@ public class Sebastian_Self_Portrait extends Frame{
      */
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-        BasicStroke bs = new BasicStroke(8.0f);
+        g2d.scale(1.5,1.5);
+        BasicStroke bs = new BasicStroke(5.0f);
         g2d.setStroke(bs);
         g2d.setFont(new Font("Arial", Font.BOLD, 25));
+        
+        //Create neck
+        Color neck_color = new Color(185,136,103);
+        GeneralPath neck_path = new GeneralPath();
+        neck_path.moveTo(277, 348);
+        neck_path.quadTo(255,323,247,284); //Head is attached here
+        neck_path.quadTo(234, 310, 219, 330);
+        neck_path.quadTo(207, 348, 183, 351);
+        neck_path.quadTo(173,354, 157,353);
+        neck_path.quadTo(149, 351, 141, 349);
+        neck_path.lineTo(158, 374);
+        neck_path.curveTo(120, 447, 168, 485, 277, 348);
+        g2d.setPaint(Color.BLACK);
+        g2d.draw(neck_path);
+        g2d.setPaint(neck_color);
+        g2d.fill(neck_path);
 
         //Create T-Shirt
         Color tshirt_color = new Color(44, 40, 45);
@@ -36,13 +53,59 @@ public class Sebastian_Self_Portrait extends Frame{
         tshirt_path.lineTo(306, 358);
         tshirt_path.quadTo(312,360, 318,364);
         tshirt_path.quadTo(334,373,362,384);
+        tshirt_path.lineTo(363, 550);
+        tshirt_path.lineTo(29, 550);
+        g2d.setPaint(Color.BLACK);
         g2d.draw(tshirt_path);
+        g2d.setPaint(tshirt_color);
+        g2d.fill(tshirt_path);
+
+        //Create Hand
+        Color hand_color = new Color(233,171,155);
+        GeneralPath hand_path = new GeneralPath();
+        hand_path.moveTo(116, 550);
+        hand_path.lineTo(82, 539);
+        hand_path.quadTo(76, 538, 68, 540);
+        hand_path.lineTo(58, 550);
+        hand_path.lineTo(116, 550);
+        g2d.setPaint(Color.BLACK);
+        g2d.draw(hand_path);
+        g2d.setPaint(hand_color);
+        g2d.fill(hand_path);
+
+        //Create Sling
+        Color sling_color = new Color(105,116,126);
+        GeneralPath sling_path = new GeneralPath();
+        sling_path.moveTo(68, 539);
+        sling_path.quadTo(93,447,121,409);
+        sling_path.quadTo(135,393,152,380);
+        sling_path.quadTo(144,401,139,412);
+        sling_path.quadTo(118,461,90,541);
+        sling_path.quadTo(76,538,68,539);
+        g2d.setPaint(Color.BLACK);
+        g2d.draw(sling_path);
+        g2d.setPaint(sling_color);
+        g2d.fill(sling_path);
+    
+        //Create Head
+        Color head_color = new Color(233,171,155);
+        GeneralPath head_path = new GeneralPath();
+        head_path.moveTo(247, 284);
+        head_path.quadTo(234, 310, 219, 330);
+        head_path.quadTo(207, 348, 183, 351);
+        head_path.quadTo(173,354, 157,353);
+        head_path.quadTo(149, 351, 141, 349);
+        g2d.setPaint(Color.GREEN);
+        g2d.draw(head_path);
+
+        
     }
     public static void main(String[] args) {
         Color backgrond_color = new Color(87, 92, 97); 
         Sebastian_Self_Portrait gp = new Sebastian_Self_Portrait();
         gp.setTitle("Sebastian Self Portrait project");
-        gp.setSize(370,569);
+        // gp.setSize(370,569);
+        gp.setSize(900,900);
         gp.setBackground(backgrond_color);
         gp.setVisible(true);
 
